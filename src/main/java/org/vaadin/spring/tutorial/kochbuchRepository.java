8464c40f7300c5427af7509dfae59eea331ef4ef
Léo <leohrbl@gmail.com>
2022-04-19 14:17:23 +0200

    Deleted all files in Repository
    Deleted all files in Repository

--- src/main/java/org/vaadin/spring/tutorial/kochbuchRepository.java
+++
@@ -1,7 +0,0 @@
-package org.vaadin.spring.tutorial;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KochbuchRepository extends CrudRepository<KochbuchEntity, Integer>{
-    
-}

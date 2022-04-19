6b80551e9e6709df4eed79c16532c61c973b836c
Léo <leohrbl@gmail.com>
2022-04-19 14:20:21 +0200

    Added correct Project Setup
    Added correct Project Setup

---
+++ src/main/java/com/example/application/MyDataSourceConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.application;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration(proxyBeanMethods = false)
+public class MyDataSourceConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("app.datasource.configuration")
+    public HikariDataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+}
